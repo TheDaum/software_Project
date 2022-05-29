c07af46d40351ffeee1900f64d8709ee1a7a9ad3
jomulagy688@gmail.com <jomulagy688@gmail.com>
2022-05-30 04:03:06 +0900

    옷장 버튼 삭제 및 추가 ver.1

--- screen/김지훈/.vscode/main.js
+++ screen/김지훈/.vscode/main.js
@@ -1,4 +1,5 @@
 var id;
+var deleteTarget;
 $('#logout').click(function () {
     logout();
 });
@@ -22,14 +23,26 @@ $('#add_clothe').click(function () {
 $('.box_area:nth-child(5)>.carousel>.carousel-inner>.carousel-item>.button_box').click(function () {
     location.href = "카테고리.html?tab=" + escape($(this).find("span").text());
 });
-$('.box_area:nth-child(8)>.carousel>.carousel-inner>.carousel-item>.button_box').click(function () {
-    var address = "옷장_detail.html?tab=" + escape($(this).find("span").text());
-    location.href = address;
+$('.box_area:nth-child(8)>.carousel>.carousel-inner>.carousel-item>.button_area>.closet_button').click(function () {
+    if ($(this).find("span").text()!=0){
+        var address = "옷장_detail.html?tab=" + escape($(this).find("span").text());
+        location.href = address;
+    }
 });
+// 옷장삭제를 누르면
+$('.box_area:nth-child(8)>.carousel>.carousel-inner>.carousel-item>.button_area>.delete_closet').click(function () {
+    deleteTarget=$(this).parent().children("button.closet_button").children('span');
+    $("#deleteMessage").text(deleteTarget.text()+"을(를) 삭제하시겠습니까?");
+});
+$('#deleteButton').click(function(){
+    deleteTarget.text("");
+})
 function logout() {
     location.replace("../../1차screen/메인new.html");
 }
-
+$('.bi').click(function () {
+    
+});
 function show_all() {
     location.href = "전체옷.html";
 }
