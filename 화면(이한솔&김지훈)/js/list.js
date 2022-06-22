1136e2b3012a309fc235c2c5eb50b3e483d31aa1
jomulagy688@gmail.com <jomulagy688@gmail.com>
2022-06-22 18:09:54 +0900

    Revert "."
    This reverts commit 6deba860eec4dd7dbe9d2db25951a280109f8537.

--- 화면(이한솔&김지훈)/js/list.js
+++ 화면(이한솔&김지훈)/js/list.js
@@ -1,127 +1,149 @@
 var result_num = 20;
-var remainder = result_num%9;
-var page_num = parseInt(result_num/9)+1;
+var remainder = result_num % 9;
+var page_num = parseInt(result_num / 9) + 1;
 var result_index = 0;
 
-var current_page=1;
+var current_page = 1;
 var userId
 var userEmail
 var clothesIden;
-var clothesarr=[];
+var clothesarr = [];
 var categoriesName;
+var clothesobj;
+var imageobj;
+var imagearr = [];
+var totalobj;
+var totalarr=[];
 
-
-$(document).ready(async function(){
-    userId=location.href
-    if(userId.indexOf("id",0)!=-1){
-        Id = userId.substring(userId.indexOf("id",0)+3);
+$(document).ready(async function () {
+    userId = location.href
+    if (userId.indexOf("id", 0) != -1) {
+        Id = userId.substring(userId.indexOf("id", 0) + 3);
     }
-    
-    console.log(Id);    
+
+
+    console.log(Id);
     axios.get('http://localhost:8080/user').then((Response) => {
         data = Response.data;
-        setTimeout(function(){
+        setTimeout(function () {
             console.log(Id)
-        },5);
-        userEmail=data[Id].userEmail
-    
+        }, 5);
+        userEmail = data[Id].userEmail
+        console.log(userEmail)
     });
 
     axios.get('http://localhost:8080/clothes').then((Response) => {
         data = Response.data;
-        for(var i = 0;i<data.length;i++){
-            if(userEmail==data[i].clothesUser){
-                clothesIden=data[i].Id
 
-                axios.get('http://localhost:8080/boardPicture').then((Response) => {
-                    data1 = Response.data;  
-                    for(var j=0; j<data1.length;j++){
-                        if(clothesIden==data1[j].Id){
-                            var clothesobj={
-                                clothesId: clothesIden,
-                                clothesName : data[i].clothesName,
-                                clothesCloset : data[i].clothesCloset,
-                                clothesCategory : data[i].clotheCategory,
-                                clothesBuy: data[i].clothesBuy,
-                                clothesSeason: data[i].clothesSeason,
-                                clothesImage: data1[i].filePath
-                            }
-                            console.log(clothesobj)
-                            clothesarr.push(clothesobj)
-                        }
-                    }
-                });
-                
-                
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].clothesUser)
+            if (userEmail == data[i].clothesUser) {
+
+                clothesIden = data[i].id
+                clothesNames = data[i].clothesName
+                clothesCategoryS = data[i].clothesCategory
+                clothesClosets = data[i].clothesCloset
+                clothesSeasons = data[i].clothesSeason
+                clothesBuys = data[i].clothesBuy
+
+                clothesobj = {
+                    clothesId: clothesIden,
+                    clothesName: clothesNames,
+                    clothesCloset: clothesClosets,
+                    clothesCategory: clothesCategoryS,
+                    clothesBuy: clothesBuys,
+                    clothesSeason: clothesSeasons,
+                }
+                clothesarr.push(clothesobj)
+
+
+
+
             }
-            
-            
+
+
         }
-        
-    
-    })
+
+
+    });
+
+    axios.get('http://localhost:8080/boardPicture').then((Response) => {
+        data = Response.data;
+
+        for (var j = 0; j < data.length; j++) {
+            imageobj = {
+                imageId: data[j].id,
+                imagepath: data[j].filePath
+            }
+            imagearr.push(imageobj)
+        }
+    });
+    setTimeout(function(){
     console.log(clothesarr)
+    console.log(imagearr)
+    },100);
+
 
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     var ul = $(".pagination");
-    for(var i = 0;i<page_num;i++){
-        if (i==0){
+    for (var i = 0; i < page_num; i++) {
+        if (i == 0) {
             continue;
         }
         var li = document.createElement("li");
         li.className = "page-item";
         ul.children(".page-item:last").before(li);
-        
+
         var a = document.createElement("a");
         a.className = "page-link";
-        a.innerText = String(i+1); 
+        a.innerText = String(i + 1);
         li.append(a);
 
         $(".pagination>.page-item:last").removeClass("disabled")
 
-        $(".page-link").click(function(){
+        $(".page-link").click(function () {
             $(".container-fluid").empty();
 
             var request_page = $(this).text();
-            
+
             $(".pagination>.page-item:first").removeClass("disabled");
             $(".pagination>.page-item:last").removeClass("disabled");
-        
-            if(request_page == 1){
+
+            if (request_page == 1) {
                 $(".pagination>.page-item:first").addClass("disabled");
             }
-            else if(request_page == parseInt(page_num)){
+            else if (request_page == parseInt(page_num)) {
                 $(".pagination>.page-item:last").addClass("disabled");
             }
             $(".pagination>.page-item").eq(current_page).removeClass("active");
             $(".pagination>.page-item").eq(request_page).addClass("active");
 
             print_card(request_page);
-            current_page=request_page;
+            current_page = request_page;
 
         });
-        
+
     }
-;
+    ;
     print_card(1);
 });
 
-function print_card(page_index){
-    if (page_index == page_num){
+function print_card(page_index) {
+    if (page_index == page_num) {
         return print_card_last();
     }
-    var result_index = 9*(page_index-1);
+    var result_index = 9 * (page_index - 1);
     var container = $(".container-fluid");
 
-    for(var i = 0; i < 3 ;i++){
-        
+    for (var i = 0; i < 3; i++) {
+
         var row = document.createElement("div");
         row.className = "row";
         container.append(row);
         row = container.children(".row").last();
-        for(var j = 0;j<3;j++){
+        for (var j = 0; j < 3; j++) {
             var area = document.createElement("div");
             area.classList.add("col-xl-4");
             area.classList.add("col-lg-7");
@@ -134,11 +156,12 @@ function print_card(page_index){
             shadow.classList.add("mb-4");
             area.append(shadow);
             shadow = area.children(".card").last();
-            
+
             var img = document.createElement("img");
             img.className = "card-img-top";
             img.classList.add("rounded");
-            $(img).attr("src","/img/background.jpg");
+            $(img).attr("src", "/img/background.jpg");
+            // img.attr("alt","...");
             shadow.append(img);
             var card = document.createElement("div");
             card.classList.add("card-body");
@@ -149,34 +172,34 @@ function print_card(page_index){
             title.className = "card-title";
             title.innerText = result_index;
             card.append(title);
-            
+
             var button = document.createElement("a");
             // button.attr("href", "#");
             button.classList.add("btn");
             button.classList.add("btn-danger");
             button.innerText = "show details";
-            $(button).attr("href","detail.html");
+            $(button).attr("href", "detail.html");
             card.append(button);
             result_index++;
         }
     }
 }
-function print_card_last(){
-    var result_index = 9*(page_num-1);
+function print_card_last() {
+    var result_index = 9 * (page_num - 1);
     var container = $(".container-fluid");
-    var row_num = parseInt(remainder/3)+1;
+    var row_num = parseInt(remainder / 3) + 1;
     var card_num = 3;
-    var last_card_num = remainder-3*(row_num-1);
-    for(var i = 0; i < row_num ;i++){  
+    var last_card_num = remainder - 3 * (row_num - 1);
+    for (var i = 0; i < row_num; i++) {
 
         var row = document.createElement("div");
         row.className = "row";
         container.append(row);
         row = container.children(".row").last();
-        if(i+1==row_num){
+        if (i + 1 == row_num) {
             card_num = last_card_num;
         }
-        for(var j = 0;j<card_num;j++){
+        for (var j = 0; j < card_num; j++) {
             var area = document.createElement("div");
             area.classList.add("col-xl-4");
             area.classList.add("col-lg-7");
@@ -189,12 +212,12 @@ function print_card_last(){
             shadow.classList.add("mb-4");
             area.append(shadow);
             shadow = area.children(".card").last();
-            
+
             var img = document.createElement("img");
             img.classList.add("card-img-top");
             img.classList.add("rounded");
-            $(img).attr("src","/img/background.jpg");
-            
+            $(img).attr("src", "/img/background.jpg");
+
             // img.attr("alt","...");
             shadow.append(img);
 
@@ -207,7 +230,7 @@ function print_card_last(){
             title.className = "card-title";
             title.innerText = result_index;
             card.append(title);
-            
+
             var button = document.createElement("a");
             // button.attr("href", "#");
             button.classList.add("btn");
@@ -220,23 +243,23 @@ function print_card_last(){
     }
 }
 
-$(".page-link").click(function(){
+$(".page-link").click(function () {
     $(".container-fluid").empty();
     var request_page = $(this).text();
-    
+
     $(".pagination>.page-item:first").removeClass("disabled");
     $(".pagination>.page-item:last").removeClass("disabled");
 
-    if(request_page == 1){
+    if (request_page == 1) {
         $(".pagination>.page-item:first").addClass("disabled");
     }
-    else if(request_page == parseInt(page_num)){
+    else if (request_page == parseInt(page_num)) {
         $(".pagination>.page-item:last").addClass("disabled");
     }
     $(".pagination>.page-item").eq(current_page).removeClass("active");
     $(".pagination>.page-item").eq(request_page).addClass("active");
 
     print_card(request_page);
-    current_page=request_page;
+    current_page = request_page;
 
 });
