8865b7584211aee7f71562ae40f8bdca2c5bfeb5
jomulagy688@gmail.com <jomulagy688@gmail.com>
2022-06-23 01:19:49 +0900

    .

--- 화면(이한솔&김지훈)/js/search.js
+++ 화면(이한솔&김지훈)/js/search.js
@@ -1,13 +1,13 @@
 var key;
 
 var result_num = 20;
-var remainder = result_num%9;
-var page_num = parseInt(result_num/9)+1;
+var remainder = result_num % 9;
+var page_num = parseInt(result_num / 9) + 1;
 var result_index = 0;
 
-var current_page=1;
+var current_page = 1;
 
-$(document).ready(function(){
+$(document).ready(function () {
     userId = unescape(location.href);
     if (userId.indexOf("id", 0) != -1) {
         Id = userId.substring(userId.indexOf("id", 0) + 3);
@@ -15,64 +15,146 @@ $(document).ready(function(){
     if (userId.indexOf("key", 0) != -1) {
         key = userId.substring(userId.indexOf("key", 0) + 4);
     }
-    
-    var ul = $(".pagination");
-    for(var i = 0;i<page_num;i++){
-        if (i==0){
-            continue;
+
+    axios.get('http://localhost:8080/user').then((Response) => {
+        data = Response.data;
+        setTimeout(function () {
+            console.log(Id)
+        }, 5);
+        userEmail = data[idd].userEmail
+        console.log(userEmail)
+    });
+
+    axios.get('http://localhost:8080/clothes').then((Response) => {
+        data = Response.data;
+
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].clothesUser)
+            if (userEmail == data[i].clothesUser) {
+
+                clothesIden = data[i].id
+                clothesNames = data[i].clothesName
+                clothesCategoryS = data[i].clothesCategory
+                clothesClosets = data[i].clothesCloset
+                clothesSeasons = data[i].clothesSeason
+                clothesBuys = data[i].clothesBuy
+
+                clothesobj = {
+                    clothesId: clothesIden,
+                    clothesName: clothesNames,
+                    clothesCloset: clothesClosets,
+                    clothesCategory: clothesCategoryS,
+                    clothesBuy: clothesBuys,
+                    clothesSeason: clothesSeasons,
+                }
+                clothesarr.push(clothesobj)
+
+
+
+
+            }
+
+
         }
-        var li = document.createElement("li");
-        li.className = "page-item";
-        ul.children(".page-item:last").before(li);
-        
-        var a = document.createElement("a");
-        a.className = "page-link";
-        a.innerText = String(i+1); 
-        li.append(a);
 
-        $(".pagination>.page-item:last").removeClass("disabled")
 
-        $(".page-link").click(function(){
-            $(".container-fluid").empty();
+    });
 
-            var request_page = $(this).text();
-            
-            $(".pagination>.page-item:first").removeClass("disabled");
-            $(".pagination>.page-item:last").removeClass("disabled");
-        
-            if(request_page == 1){
-                $(".pagination>.page-item:first").addClass("disabled");
+    axios.get('http://localhost:8080/boardPicture').then((Response) => {
+        data = Response.data;
+
+        for (var j = 0; j < data.length; j++) {
+            imageobj = {
+                imageId: data[j].id,
+                imagepath: data[j].filePath
             }
-            else if(request_page == parseInt(page_num)){
-                $(".pagination>.page-item:last").addClass("disabled");
+            imagearr.push(imageobj)
+        }
+    });
+    setTimeout(function () {
+
+        for (var i = 0; i < clothesarr.length; i++) {
+            for (var j = 0; j < imagearr.length; j++) {
+
+                if (clothesarr[i].clothesName.indexOf(key, 0) != -1 && clothesarr[i].clothesId == imagearr[j].imageId) {
+                    totalobj = {
+                        totalId: clothesarr[i].clothesId,
+                        totalName: clothesarr[i].clothesName,
+                        totalPath: imagearr[j].imagepath
+                    }
+                    totalarr.push(totalobj)
+
+
+
+                }
             }
-            $(".pagination>.page-item").eq(current_page).removeClass("active");
-            $(".pagination>.page-item").eq(request_page).addClass("active");
 
-            print_card(request_page);
-            current_page=request_page;
+        }
+    }, 100);
 
-        });
-        
-    }
-;
-    print_card(1);
+    setTimeout(function () {
+        result_num = totalarr.length;
+        remainder = result_num % 9;
+        page_num = parseInt(result_num / 9) + 1;
+
+        var ul = $(".pagination");
+        for (var i = 0; i < page_num; i++) {
+            if (i == 0) {
+                continue;
+            }
+            var li = document.createElement("li");
+            li.className = "page-item";
+            ul.children(".page-item:last").before(li);
+
+            var a = document.createElement("a");
+            a.className = "page-link";
+            a.innerText = String(i + 1);
+            li.append(a);
+
+            $(".pagination>.page-item:last").removeClass("disabled")
+
+            $(".page-link").click(function () {
+                $(".container-fluid").empty();
+
+                var request_page = $(this).text();
+
+                $(".pagination>.page-item:first").removeClass("disabled");
+                $(".pagination>.page-item:last").removeClass("disabled");
+
+                if (request_page == 1) {
+                    $(".pagination>.page-item:first").addClass("disabled");
+                }
+                else if (request_page == parseInt(page_num)) {
+                    $(".pagination>.page-item:last").addClass("disabled");
+                }
+                $(".pagination>.page-item").eq(current_page).removeClass("active");
+                $(".pagination>.page-item").eq(request_page).addClass("active");
+
+                print_card(request_page);
+                current_page = request_page;
+
+            });
+
+        }
+
+        print_card(1);
+    }, 100);
 });
 
-function print_card(page_index){
-    if (page_index == page_num){
+function print_card(page_index) {
+    if (page_index == page_num) {
         return print_card_last();
     }
-    var result_index = 9*(page_index-1);
+    var result_index = 9 * (page_index - 1);
     var container = $(".container-fluid");
 
-    for(var i = 0; i < 3 ;i++){
-        
+    for (var i = 0; i < 3; i++) {
+
         var row = document.createElement("div");
         row.className = "row";
         container.append(row);
         row = container.children(".row").last();
-        for(var j = 0;j<3;j++){
+        for (var j = 0; j < 3; j++) {
             var area = document.createElement("div");
             area.classList.add("col-xl-4");
             area.classList.add("col-lg-7");
@@ -85,12 +167,16 @@ function print_card(page_index){
             shadow.classList.add("mb-4");
             area.append(shadow);
             shadow = area.children(".card").last();
-            
+
             var img = document.createElement("img");
             img.className = "card-img-top";
             img.classList.add("rounded");
-            $(img).attr("src","/img/background.jpg");
-            // img.attr("alt","...");
+
+
+
+            var path = "http://localhost:8080/files/1dddddd.jpg";
+            $(img).attr("src", path);
+
             shadow.append(img);
             var card = document.createElement("div");
             card.classList.add("card-body");
@@ -99,37 +185,35 @@ function print_card(page_index){
 
             var title = document.createElement("h5");
             title.className = "card-title";
-            title.innerText = result_index;
+            title.innerText = totalName;
             card.append(title);
-            
+
             var button = document.createElement("a");
-            // button.attr("href", "#");
+            $(button).attr("href", "detail.html?id=" + idd + "&?clothesId=" + totalarr[result_index++].totalId);
             button.classList.add("btn");
             button.classList.add("btn-danger");
             button.innerText = "show details";
-            $(button).attr("href","detail.html");
+            $(button).attr("href", "detail.html?id=" + idd + "&?clothesId=" + totalarr[result_index++].totalId);
             card.append(button);
-            result_index++;
         }
     }
 }
-
-function print_card_last(){
-    var result_index = 9*(page_num-1);
+function print_card_last() {
+    var result_index = 9 * (page_num - 1);
     var container = $(".container-fluid");
-    var row_num = parseInt(remainder/3)+1;
+    var row_num = parseInt(remainder / 3) + 1;
     var card_num = 3;
-    var last_card_num = remainder-3*(row_num-1);
-    for(var i = 0; i < row_num ;i++){  
+    var last_card_num = remainder - 3 * (row_num - 1);
+    for (var i = 0; i < row_num; i++) {
 
         var row = document.createElement("div");
         row.className = "row";
         container.append(row);
         row = container.children(".row").last();
-        if(i+1==row_num){
+        if (i + 1 == row_num) {
             card_num = last_card_num;
         }
-        for(var j = 0;j<card_num;j++){
+        for (var j = 0; j < card_num; j++) {
             var area = document.createElement("div");
             area.classList.add("col-xl-4");
             area.classList.add("col-lg-7");
@@ -142,13 +226,14 @@ function print_card_last(){
             shadow.classList.add("mb-4");
             area.append(shadow);
             shadow = area.children(".card").last();
-            
+
             var img = document.createElement("img");
             img.classList.add("card-img-top");
             img.classList.add("rounded");
-            $(img).attr("src","/img/background.jpg");
-            
-            // img.attr("alt","...");
+            var path = "http://localhost:8080/files/1dddddd.jpg";
+            $(img).attr("src", path);
+
+
             shadow.append(img);
 
             var card = document.createElement("div");
@@ -160,38 +245,36 @@ function print_card_last(){
             title.className = "card-title";
             title.innerText = result_index;
             card.append(title);
-            
+
             var button = document.createElement("a");
-            // button.attr("href", "#");
+
+            $(button).attr("href", "detail.html?id=" + idd + "&?clothesId=" + totalarr[result_index++].totalId);
             button.classList.add("btn");
             button.classList.add("btn-danger");
             button.innerText = "show details";
             card.append(button);
-            result_index++;
 
         }
     }
 }
 
-
-$(".page-link").click(function(){
+$(".page-link").click(function () {
     $(".container-fluid").empty();
     var request_page = $(this).text();
-    
+
     $(".pagination>.page-item:first").removeClass("disabled");
     $(".pagination>.page-item:last").removeClass("disabled");
 
-    if(request_page == 1){
+    if (request_page == 1) {
         $(".pagination>.page-item:first").addClass("disabled");
     }
-    else if(request_page == parseInt(page_num)){
+    else if (request_page == parseInt(page_num)) {
         $(".pagination>.page-item:last").addClass("disabled");
     }
     $(".pagination>.page-item").eq(current_page).removeClass("active");
     $(".pagination>.page-item").eq(request_page).addClass("active");
 
     print_card(request_page);
-    current_page=request_page;
-    
-});
+    current_page = request_page;
 
+});
